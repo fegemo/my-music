4399309cddd062e61a2e6d1ae3dae5c1323fee11
Flavio Coutinho <fegemo@gmail.com>
2017-03-08 12:26:24 -0300

    [Atividade 3.7] altera playlists de 6 para 3

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,12 +43,9 @@ angular.module('music.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Power Metal', id: 1 },
+    { title: 'Space Metal', id: 2 },
+    { title: 'Rock', id: 3 },
   ];
 })
 
